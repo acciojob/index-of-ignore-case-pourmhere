6a2e1c50b032f82080b486dd704450a236de4b17
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-07-15 11:26:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	if (!s1 || !s2) return -1; // Return -1 if either input string is empty
+  const lowerCaseStr = s1.toLowerCase(); // Convert the original string to lowercase
+  const lowerCaseSubStr = s2.toLowerCase(); // Convert the substring to lowercase
+  return lowerCaseStr.indexOf(lowerCaseSubStr);
 }
 
 // Please do not change the code below
